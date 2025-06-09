5e332fa0ed83cd06faa8c8ff7c999f3728395979
Malik Elmessiry <malik.messiry@gmail.com>
2025-06-09 22:55:32 +0200

    setup

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ const App = () => {
   return (
     <div id="App">
       <header>
-        <h1>Application title</h1>
+        <h1>Chat Log</h1>
       </header>
       <main>
         {/* Wave 01: Render one ChatEntry component
